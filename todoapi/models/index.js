f1c1687728eae82c94347f8ce87758faeecd5e98
Nalipp <nateplipp@gmail.com>
2017-12-23 12:24:17 -0600

    add mlab

--- todoapi/models/index.js
+++ todoapi/models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+// mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://heroku_brsv3rk0:or3qfqhoglj2n80o5fp6c2cau@ds163826.mlab.com:63826/heroku_brsv3rk0');
 
 mongoose.Promise = Promise;
 
