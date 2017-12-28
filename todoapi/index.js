41f2b4f5a9470794c40fec61307be65ba610ba3c
Nalipp <nateplipp@gmail.com>
2017-12-28 13:53:57 -0600

    TodoList

--- todoapi/index.js
+++ todoapi/index.js
@@ -9,12 +9,12 @@ var todoRoutes = require("./routes/todos");
 app.use(express.static(path.resolve(__dirname, '../todosfrontend/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname +'/public'));
-app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname +'/public'));
+// app.use(express.static(__dirname + '/views'));
 
-app.get('/', function(req, res){
-    res.sendFile("index.html");
-});
+// app.get('/', function(req, res){
+//     res.sendFile("index.html");
+// });
 
 app.use('/api/todos', todoRoutes);
 
